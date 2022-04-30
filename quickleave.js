6114a9a2c2ee094597012fa72560189df7317fdf
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-04-30 13:02:11 -0500

    soup

--- quickleave.js
+++ quickleave.js
@@ -1,29 +1,5 @@
-function amazing() {
-   window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
+window.addEventListener('keydown', function (e) {
+    if (e.key == "p") {
+      location.replace("https://classroom.google.com/u/0/")
     }
-  
-    switch (event.key) {
-      case "p":
-        this.location.replace(https://classroom.google.com/u/0/)
-        break;
-      case "":
-        // code for "up arrow" key press.
-        break;
-      case "":
-        // code for "left arrow" key press.
-        break;
-      case "":
-        // code for "right arrow" key press.
-        break;
-      default:
-        return; // Quit when this doesn't handle the key event.
-    }
-  
-    // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-  }, true);
-  // the last option dispatches the event to the listener first,
-  // then dispatches event to window 
-}
+  }, false);
