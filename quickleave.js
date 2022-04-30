741bde947b5bbbd99c4d30953729f8c598205005
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-04-30 13:03:00 -0500

    sop

--- quickleave.js
+++ quickleave.js
@@ -1,5 +1,7 @@
-window.addEventListener('keydown', function (e) {
+function amazing() {
+   window.addEventListener('keydown', function (e) {
     if (e.key == "p") {
       location.replace("https://classroom.google.com/u/0/")
     }
-  }, false);
+  }, false); 
+}
