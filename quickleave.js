1fcb6ece7cf23dcb32723f2b22fe93aca8652c8f
WyattJP <79605302+WyattJPrice@users.noreply.github.com>
2023-03-09 14:34:25 -0600

    d

--- quickleave.js
+++ quickleave.js
@@ -8,11 +8,11 @@ function amazing() {
 
 function shield(url) {
   var win = window.open()
-var url = url
-var iframe = win.document.createElement('iframe')
-iframe.style.width = "100%";
-iframe.style.height = "100%";
-iframe.style.border = "none";
-iframe.src = url
-win.document.body.appendChild(iframe)
+  var url = url
+  var iframe = win.document.createElement('iframe')
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.src = url
+  win.document.body.appendChild(iframe)
 }
