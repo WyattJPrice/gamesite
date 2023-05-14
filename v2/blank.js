a913721dc5eb19c5b6aa97a5640440388669773e
WyattJP <79605302+WyattJPrice@users.noreply.github.com>
2023-05-14 15:32:37 -0500

    d

---
+++ v2/blank.js
@@ -0,0 +1,43 @@
+function urlhide(url) {
+  var win = window.open()
+  var url = url
+  var iframe = win.document.createElement('iframe')
+  iframe.style.position = "fixed";
+  iframe.style.top = "0%";
+  iframe.style.left = "0";
+  iframe.style.bottom = "0";
+  iframe.style.right = "0";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.style.margin = "0";
+  iframe.style.padding = "0";
+  iframe.style.overflow = "hidden";
+  iframe.src = url
+  win.document.body.appendChild(iframe)
+}
+
+var element = document.getElementById("imageid");
+element.addEventListener("click", function () {
+  urlhide("https://paper-io.com/")
+})
+
+var element = document.getElementById("imageid1");
+element.addEventListener("click", function () {
+  urlhide("https://www.google.com/logos/2019/july4th19/r6/july4th19.html?hl=en")
+})
+
+var element = document.getElementById("imageid2");
+element.addEventListener("click", function () {
+  urlhide("https://littlealchemy.com/")
+})
+
+var element = document.getElementById("imageid3");
+element.addEventListener("click", function () {
+  urlhide("https://html5.gamemonetize.co/ugi7ftbv2kgodcq7vful9u9v34wein5z/")
+})
+
+var element = document.getElementById("private");
+element.addEventListener("click", function () {
+  urlhide("https://paper-io.com/ps/?p-us1qjihoel")
+})
---
+++ v2/quickleave.js
@@ -0,0 +1,7 @@
+function amazing() {
+   window.addEventListener('keydown', function (e) {
+    if (e.key == "p") {
+      location.replace("https://classroom.google.com/u/0/")
+    }
+  }, false); 
+}
